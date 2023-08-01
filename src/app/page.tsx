644f1308781225148af0cc363b7a79c223a46c94
l-palermo <l-palermo@hotmail.it>
2023-08-01 19:27:44 +0200

    fix: links to open on new tabs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,9 +8,6 @@ import Link from 'next/link';
 
 export default function Home() {
   const { contacts, headerGroup, footerAriaLabel } = config;
-  const isMobile =
-  typeof window !== 'undefined' &&
-    (window.navigator.userAgent.toLowerCase().includes('mobile') || window.navigator.maxTouchPoints >= 1 || true);
   return (
     <div className={styles.root}>
       <hgroup className={styles['header-group']}>
@@ -22,15 +19,13 @@ export default function Home() {
       <main className={styles['main']}></main>
       <footer className={styles['footer']} aria-label={footerAriaLabel}>
         <ul className={styles['footer__contacts']} aria-label={contacts.ariaLabel}>
-          {contacts.items.map(({ Icon, href, ariaLabel }) =>
-            href.startsWith('tel:') && !isMobile ? null : (
-              <li key={ariaLabel} className={styles['footer__contacts__item']}>
-                <Link href={href} aria-label={ariaLabel}>
-                  <Icon className={styles['footer__contacts__item__icon']} aria-hidden data-testid={ariaLabel} />
-                </Link>
-              </li>
-            ),
-          )}
+          {contacts.items.map(({ Icon, href, ariaLabel }) => (
+            <li key={ariaLabel} className={styles['footer__contacts__item']}>
+              <Link href={href} aria-label={ariaLabel} target="_blank" rel="noopener noreferrer">
+                <Icon className={styles['footer__contacts__item__icon']} aria-hidden data-testid={ariaLabel} />
+              </Link>
+            </li>
+          ))}
         </ul>
       </footer>
     </div>
