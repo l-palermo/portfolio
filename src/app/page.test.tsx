0b068debaa33f0419b6d7469fb1338ad4019d975
l-palermo <l-palermo@hotmail.it>
2023-08-01 19:01:37 +0200

    chore: add test setup, jest and recat testing library

---
+++ src/app/page.test.tsx
@@ -0,0 +1,31 @@
+import { render, screen, within } from '@testing-library/react';
+
+import Portfolio from './page';
+import config from '../lib/config';
+
+describe('Portfolio', () => {
+  it('should render the header, subhaeder, description and an icon hidden to sr', () => {
+    render(<Portfolio />);
+
+    expect(screen.getByRole('heading', { level: 1, name: config.headerGroup.header })).toBeVisible();
+    expect(screen.getByRole('heading', { level: 2, name: config.headerGroup.subHeader })).toBeVisible();
+    expect(screen.getByText(config.headerGroup.description)).toBeVisible();
+    expect(screen.getByTestId('header-icon')).toHaveAttribute('aria-hidden', 'true');
+  });
+
+  it('should render the footer containing the contacts section and links', () => {
+    render(<Portfolio />);
+
+    const footer = screen.getByRole('contentinfo', { name: config.footerAriaLabel });
+    const contactDetailsList = within(footer).getByRole('list', { name: config.contacts.ariaLabel });
+    const links = within(contactDetailsList).getAllByRole('link');
+
+    expect(links).toHaveLength(config.contacts.items.length);
+    links.forEach((link, index) => {
+      const { href, ariaLabel } = config.contacts.items[index];
+      expect(link).toHaveAttribute('aria-label', ariaLabel);
+      expect(link).toHaveAttribute('href', href);
+      expect(screen.getByTestId(ariaLabel)).toHaveAttribute('aria-hidden', 'true');
+    });
+  });
+});
--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,37 +1,37 @@
-import TelephoneIcon from "../assets/svgs/telephone.svg";
-import MailIcon from "../assets/svgs/mail.svg";
-import LinkedinIcon from "../assets/svgs/linkedin-logo.svg";
-import GithubIcon from "../assets/svgs/github-logo.svg";
+import TelephoneIcon from '../assets/svgs/telephone.svg';
+import MailIcon from '../assets/svgs/mail.svg';
+import LinkedinIcon from '../assets/svgs/linkedin-logo.svg';
+import GithubIcon from '../assets/svgs/github-logo.svg';
 
 const config = {
   headerGroup: {
-    hedaer: "luigi palermo",
-    subHeader: "frontend engineer",
-    description:
-      "I build accessible, inclusive products and digital experiences for the web.",
+    header: 'luigi palermo',
+    subHeader: 'frontend engineer',
+    description: 'I build accessible, inclusive products and digital experiences for the web.',
   },
+  footerAriaLabel: 'additional information', 
   contacts: {
-    ariaLabel: "contacts",
+    ariaLabel: 'contact details',
     items: [
       {
         Icon: TelephoneIcon,
-        href: "tel:+447490393368",
-        ariaLabel: "phone number",
+        href: 'tel:+447490393368',
+        ariaLabel: 'phone number',
       },
       {
         Icon: MailIcon,
-        href: "mailto:l-palermo@hotmail.it",
-        ariaLabel: "email",
+        href: 'mailto:l-palermo@hotmail.it',
+        ariaLabel: 'email',
       },
       {
         Icon: LinkedinIcon,
-        href: "https://www.linkedin.com/in/luigi-palermo-b09733a0/",
-        ariaLabel: "linkedin profile",
+        href: 'https://www.linkedin.com/in/luigi-palermo-b09733a0/',
+        ariaLabel: 'linkedin profile',
       },
       {
         Icon: GithubIcon,
-        href: "https://github.com/l-palermo",
-        ariaLabel: "github profile",
+        href: 'https://github.com/l-palermo',
+        ariaLabel: 'github profile',
       },
     ],
   },
