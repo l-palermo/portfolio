075ee1b28a93395fb42958b6d558db5dd3315694
l-palermo <l-palermo@hotmail.it>
2023-08-01 15:52:48 +0200

    chore: add svgr to transform svg in react components, and add eslint and prettier

--- next.config.js
+++ next.config.js
@@ -1,25 +1,5 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    webpack(config) {
-        const fileLoaderRule = config.module.rules.find((rule) =>
-          rule.test?.test?.('.svg'),
-        )
-        config.module.rules.push(
-          {
-            ...fileLoaderRule,
-            test: /\.svg$/i,
-            resourceQuery: /url/,
-          },
-          {
-            test: /\.svg$/i,
-            resourceQuery: { not: /url/ },
-            use: ['@svgr/webpack'],
-          },
-        )
-        fileLoaderRule.exclude = /\.svg$/i
-        return config
-      },
-}
 
+const withSvgr = require("next-plugin-svgr");
 
-module.exports = {}
+module.exports = withSvgr();
