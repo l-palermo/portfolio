b8852cfa2135a5f2e80df467d1e1bc1e0f79db87
l-palermo <l-palermo@hotmail.it>
2023-10-08 23:08:45 +0100

    feat: update color tokens and add analytics

--- src/app/(routes)/layout.tsx
+++ src/app/(routes)/layout.tsx
@@ -1,6 +1,8 @@
 import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+import { Analytics } from '@vercel/analytics/react';
+
 
 import { Layout } from '../_/components/home-page';
 
@@ -16,6 +18,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="en">
       <body className={inter.className}>
         <Layout>{children}</Layout>
+        <Analytics />
       </body>
     </html>
   );
